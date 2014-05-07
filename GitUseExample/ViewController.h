4ef50568f556daf75e08200fb83d572797f52b3c
Keith Nagle <keith@skoogmusic.com>
2014-05-07 15:52:44 +0100

    Initial Commit

---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Keith Nagle on 07/05/2014.
+//  Copyright (c) 2014 Skoogmusic Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitUseExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitUseExample
+//
+//  Created by Keith Nagle on 07/05/2014.
+//  Copyright (c) 2014 Skoogmusic Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
